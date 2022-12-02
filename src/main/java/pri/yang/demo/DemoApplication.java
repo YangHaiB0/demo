fcd27c3ee24d59be2abbfdbec64836236121542b
杨海波 <yanghaibo@gzhengdian.com>
2022-12-02 17:12:57 +0800

    feature(init): 初始化

---
+++ src/main/java/pri/yang/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package pri.yang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动器
+ *
+ * @author Yang
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
